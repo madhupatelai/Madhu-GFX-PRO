ad04a897f60fb2376bced61c91acdc493162640a
madhupatelai <madhupatelpspk@gmail.com>
2026-09-02 12:25:06 +0530

    Update MainActivity.kt

--- app/src/main/Java/com/madhugfxpro/app/MainActivity.kt
+++ app/src/main/Java/com/madhugfxpro/app/MainActivity.kt
@@ -3,7 +3,6 @@ package com.madhugfxpro.app
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
